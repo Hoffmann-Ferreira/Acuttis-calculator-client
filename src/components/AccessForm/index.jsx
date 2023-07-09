23e0587bb26aca4e0ed76e46d28107dbb26a6b4d
Hoffmann-Ferreira <andreihf@outlook.com>
2023-07-09 01:01:59 -0300

    feat: login page and its components

---
+++ src/components/AccessForm/index.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import api from '../../services/api';
+
+const AccessForm = (props) => {
+  const navigate = useNavigate();
+  const resolver = yupResolver(props.schema);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver });
+
+  const handleAcess = (data) => {
+    api
+      .post(props.rote, data)
+      .then((res) => {
+        console.log('data', res.data);
+        console.log('token', res.data.token);
+      })
+      .catch((err) => {
+        console.log('deu ruim', err);
+      });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(handleAcess)}>
+        <fieldset>
+          {props.registering && (
+            <>
+              <label>Nome:</label>
+              <input
+                type='text'
+                placeholder='Digite seu nome de usuário'
+                {...register('name', { required: true })}
+              />
+              <p>{errors?.name?.message}</p>
+            </>
+          )}
+          <label>Endereço de email:</label>
+          <input
+            type='text'
+            placeholder='Digite seu email'
+            {...register('email', { required: true })}
+          />
+          <p>{errors?.email?.message}</p>
+          <label>Digite sua senha:</label>
+          {props.registering && (
+            <p>
+              A senha deve conter no mínimo 8 caracteres e incluir pelo menos
+              uma letra maiúscula um dígito e caracter especial
+            </p>
+          )}
+          <input
+            type='tetxt'
+            placeholder='Digite sua senha'
+            {...register('senha', { required: true })}
+          />
+          <p>{errors?.senha?.message}</p>
+          <div>
+            {props.registering ? (
+              <>
+                <button type='submit'>Salvar</button>
+                <button onClick={() => navigate('/')}>
+                  Retornar para login
+                </button>
+              </>
+            ) : (
+              <>
+                <button type='submit'>Login</button>
+                <button onClick={() => navigate('/registrar')}>
+                  Cadastrar-se
+                </button>
+              </>
+            )}
+          </div>
+        </fieldset>
+      </form>
+    </>
+  );
+};
+
+export default AccessForm;
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import * as yup from 'yup';
+import AccessForm from '../../components/AccessForm';
+
+const loginSchema = yup.object({
+    email: yup
+      .string()
+      .email('email inválido')
+      .required('Preenchimento obrigatório do email'),
+    senha: yup
+      .string()
+      .matches(
+        /((?=.*\d)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+        'Senha incorreta!'
+      ).required('Preenchimento obrigatório da senha!'),
+  });
+
+  const Login = () => {
+    return(
+        <>
+        <header>
+            <h1>Bem vindo a calculadora de jornada de trabalho Acuttis</h1>
+        </header>
+        <main>
+            <AccessForm schema={loginSchema} rote='' registering={false}/>
+        </main>
+        </>
+    )
+  }
+
+  export default Login;
